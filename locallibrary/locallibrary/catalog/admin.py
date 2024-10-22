575e10aa1e6f373ba20b62d202b8d5a8f6b98cdd
Владислав Стретович <gr424_stvev@lab-402-1.tomtit-tomsk.ru>
2024-10-22 13:08:46 +0700

    first commit

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Author, Genre,Book, BookInstance
 
-# Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
